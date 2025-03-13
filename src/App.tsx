f5ce6478f3d0ba79390c4c698ddddcf4656c606d
Paul Cookie <paul.pechenin@gmail.com>
2025-03-13 16:28:14 +0000

    disable FPS stats

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import FPSStats from 'react-fps-stats'
+// import FPSStats from 'react-fps-stats'
 
 import Mapbox from './Mapbox.tsx';
 
@@ -6,13 +6,6 @@ export default function App() {
   return (
     <div className="relative w-full h-full">
       <Mapbox />
-
-      <FPSStats
-        bottom='auto'
-        left={0}
-        top={0}
-        right='auto'
-      />
     </div>
   );
 }
