99680310907fcaa86574253507eaeb0329f44bb8
serhiy-andreiev <serhiy.andrejev@gmail.com>
2025-04-25 07:11:36 +0200

    maxAge up

--- src/constants/layer/wind.ts
+++ src/constants/layer/wind.ts
@@ -109,7 +109,7 @@ export const getWindLayers = (layersState: LayersState): Layer[] => [
     imageUnscale: BASE.IMAGE_UNSCALE,
     bounds: BASE.WIND_MAP_BOUNDS,
     numParticles: 5000,
-    maxAge: 40,
+    maxAge: 60,
     speedFactor: 10,
     width: 3,
     opacity: 0.1,
