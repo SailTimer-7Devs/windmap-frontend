3e35780c124c0d392b75e9c4e4d397a227875039
dm <3004dm@gmail.com>
2025-10-20 14:35:01 +0300

    [updated]: env.example

--- src/lib/cookies.ts
+++ src/lib/cookies.ts
@@ -1,8 +1,8 @@
 const API_URL = import.meta.env.VITE_API_URL
 
-// if (!API_URL) {
-//   throw new Error('Missing env variables: API_URL')
-// }
+if (!API_URL) {
+  throw new Error('Missing env variables: API_URL')
+}
 
 export async function getCookies(accessToken: string): Promise<void> {
   if (!accessToken) {
