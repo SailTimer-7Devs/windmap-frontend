f2477fc8841d04292947aa800d3c04d54815380f
Nataliia Chernieva <74625141+NataliiaChernieva@users.noreply.github.com>
2025-10-20 16:38:43 +0300

    UTC to currentTimezone time

--- src/components/Mapbox/TimelineControl.tsx
+++ src/components/Mapbox/TimelineControl.tsx
@@ -1,9 +1,12 @@
 import type { TimelineControlConfig } from 'weatherlayers-gl'
 
+import { formatDateToLocalISOString } from 'lib/date'
+
 import React from 'react'
 import * as WeatherLayers from 'weatherlayers-gl'
 
 const TimelineControl = (props: TimelineControlConfig): null => {
+  const { datetime, datetimes } = props
   React.useEffect(() => {
     const timelineControl = new WeatherLayers.TimelineControl({ ...props })
 
@@ -15,7 +18,18 @@ const TimelineControl = (props: TimelineControlConfig): null => {
       timelineControl.remove()
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [props.datetimes])
+  }, [datetimes])
+
+  React.useEffect(() => {
+    const current = document.getElementsByClassName('weatherlayers-timeline-control__current-datetime')[0] as HTMLElement
+    const startDate = document.getElementsByClassName('weatherlayers-timeline-control__start-datetime')[0] as HTMLElement
+    const endDate = document.getElementsByClassName('weatherlayers-timeline-control__end-datetime')[0] as HTMLElement
+
+    startDate.innerText = formatDateToLocalISOString(datetimes[0])
+    endDate.innerText = formatDateToLocalISOString(datetimes[datetimes.length - 1])
+    current.innerText = formatDateToLocalISOString(datetime)
+
+  }, [datetime])
 
   return null
 }
