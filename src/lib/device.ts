9550df279d07c9b35428c0f1b9f8aae9fcc68001
alice29shevchuk <Sevc_sd76@student.itstep.org>
2025-12-17 17:53:36 +0200

    SAIL-144: Fix

--- src/lib/device.ts
+++ src/lib/device.ts
@@ -3,15 +3,13 @@ import { UAParser } from 'ua-parser-js'
 const parcer = new UAParser()
 
 export const deviceType = parcer.getDevice().type
-export const deviceOsName = parcer.getOS().name
 
-export const isAndroid = deviceOsName === 'Android'
+export const device = await parcer.getDevice().withFeatureCheck()
+export const os = parcer.getOS()
 
-const isIpad =
-    deviceOsName === 'macOS' &&
-    navigator.maxTouchPoints > 1
+export const isAndroid = device.is('Android')
 
 export const isMobile =
-    deviceType === 'mobile' ||
-    deviceType === 'tablet' ||
-    isIpad
+    device.is('iPhone') ||
+    device.is('iPad') ||
+    isAndroid 
