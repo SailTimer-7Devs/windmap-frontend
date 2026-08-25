ec7d2b7a2721a03b5e4a30584af8b165b2a5935e
mogw <mogwinfo@gmail.com>
2026-08-25 10:33:52 -0500

    Add responsive WNI hourly forecasts and restore tap tooltips across all WNI map layers

--- src/bootstrap.tsx
+++ src/bootstrap.tsx
@@ -5,6 +5,7 @@ import { createBrowserRouter } from 'react-router'
 
 import Application from './App'
 import routes from 'routes'
+import WeatherOverlay from 'components/WeatherOverlay/WeatherOverlay'
 
 export const client = new QueryClient({
   defaultOptions: {
@@ -17,6 +18,14 @@ export const client = new QueryClient({
 export const isDevMode = import.meta.env.MODE === 'development'
 
 export const router: RouterProviderProps['router'] = createBrowserRouter([
+  {
+    path: '/overlay/weather',
+    element: <WeatherOverlay />
+  },
+  {
+    path: '/poc/inavx-overlay',
+    element: <WeatherOverlay />
+  },
   {
     path: '/',
     element: <Application />,
